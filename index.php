2608657c7e6d098b9957cf0b02037dea046f5c43
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-14 14:26:56 +0100

    ex 5

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>PHP fonctions</title>
+    </head>
+    <body>
+<?php
+    function melting($string1, $one){
+            $result = $string1. ' '. $one;
+            return $result;
+        }
+       echo melting('hello',18);
+?>
+    </body>
+</html>
